4a75e4825530fc432b33b7cf6a0db82423e56d40
gustavomurar <gustavomurarosoares3@gmail.com>
2024-07-12 15:10:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue' ;
+</script>
+
+<template>
+  <h1>Componentes</h1>
+  <hr>
+  <ExpandBox title="Caixa 1" content="Conteúdo secreto da caixa 1"/>
+  <expand-box title="Caixa 2" content="Conteúdo secreto da caixa 2"/>
+</template>
+
+<style scoped>
+
+</style>
